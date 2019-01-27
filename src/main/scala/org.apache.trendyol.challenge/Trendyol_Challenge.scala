86707171bcb888477afe2c5edefcae89aed694b9
doguscamangaoglu <doguscamangaoglu@gmail.com>
2019-01-27 19:49:26 +0300

    Değişiklikler

--- src/main/scala/org.apache.trendyol.challenge/Trendyol_Challenge.scala
+++ src/main/scala/org.apache.trendyol.challenge/Trendyol_Challenge.scala
@@ -33,6 +33,19 @@ object Trendyol_Challenge {
       .field("userID", Types.STRING)
       .build
 
+    //resourcedan csv alma
+
+//    val csvtable = CsvTableSource
+//      .builder
+//      .path(getClass.getResource("/input/case.csv").getPath)
+//      .ignoreFirstLine
+//      .fieldDelimiter("|")
+//      .field("date", Types.STRING)
+//      .field("productID", Types.STRING)
+//      .field("eventName", Types.STRING)
+//      .field("userID", Types.STRING)
+//      .build
+
 
 
     tableEnv.registerTableSource("Challenge_Csv", csvtable)
@@ -43,29 +56,29 @@ object Trendyol_Challenge {
     //result 1 v.2
     //val result1=tableEnv.scan("Challenge_Csv").where("eventName='view'").groupBy("productID").select("productID,count(1)")
 
-    result1.toDataSet[(String,Long)].writeAsCsv("/Trendyol_Challenge/output/result1.txt",fieldDelimiter = "|",writeMode = WriteMode.OVERWRITE).setParallelism(1)
+    result1.toDataSet[(String,Long)].writeAsCsv("/result/result1.txt",fieldDelimiter = "|",writeMode = WriteMode.OVERWRITE).setParallelism(1)
     //result 2
     val result2 = tableEnv.sqlQuery("select eventName,count(1) from Challenge_Csv group by eventName ")
     //result 2 v.2
     //val result2=tableEnv.scan("Challenge_Csv").groupBy("eventName").select("eventName,count(1)")
-    result2.toDataSet[(String,Long)].writeAsCsv("/Trendyol_Challenge/output/result2.txt",fieldDelimiter = "|",writeMode = WriteMode.OVERWRITE).setParallelism(1)
+    result2.toDataSet[(String,Long)].writeAsCsv("/result/result2.txt",fieldDelimiter = "|",writeMode = WriteMode.OVERWRITE).setParallelism(1)
 
     //result 3
     val result3 = tableEnv.sqlQuery("select userID from Challenge_Csv group by userID having count(distinct eventName)=4 order by 1 desc limit 5")
 
-    result3.toDataSet[Row].writeAsCsv("/Trendyol_Challenge/output/result3.txt",fieldDelimiter = "|",writeMode = WriteMode.OVERWRITE).setParallelism(1)
+    result3.toDataSet[Row].writeAsText("/result/result3.txt").setParallelism(1)
 
     //result 4
     val result4 = tableEnv.sqlQuery("select eventName,count(1) from Challenge_Csv where userID='47' group by  eventName")
     //result 4 v2
     //val result4=tableEnv.scan("Challenge_Csv").where("userID='47'").groupBy("eventName").select("eventName,count(1)")
-    result4.toDataSet[(String,Long)].writeAsCsv("/Trendyol_Challenge/output/result4.txt",fieldDelimiter = "|",writeMode = WriteMode.OVERWRITE).setParallelism(1)
+    result4.toDataSet[(String,Long)].writeAsCsv("/result/result4.txt",fieldDelimiter = "|",writeMode = WriteMode.OVERWRITE).setParallelism(1)
 
     //reult 5
     val result5 = tableEnv.sqlQuery("select distinct productID from Challenge_Csv where userID='47' ")
     //result 5 v2
     //val result5=tableEnv.scan("Challenge_Csv").where("userID='47'").groupBy("productID").select(" productID")
-    result5.toDataSet[Row].writeAsText("/Trendyol_Challenge/output/result5.txt",writeMode = WriteMode.OVERWRITE).setParallelism(1)
+    result5.toDataSet[Row].writeAsText("/result/result5.txt",writeMode = WriteMode.OVERWRITE).setParallelism(1)
 
 
 
